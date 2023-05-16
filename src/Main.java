387a1ce959a8e0ae465b49576a781cb460a3029b
KsuKravchuk <burnoska@gmail.com>
2023-05-16 22:05:44 +0200

    first commit

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,8 @@ public class Main {
             System.out.println(array[i]);
             // можно ли сделать это в одном цикле???
 
+
+
         }
 //        int result = 0;
 //        int middle = array.length / 2;
